f4e1d69c2d0462249aeeeb1729a536cc4372e15d
James McMahon <whichoneiwonder@gmail.com>
2015-09-20 14:08:12 +1000

    Expanded test array

--- sms_sender/lib/sms_sender.rb
+++ sms_sender/lib/sms_sender.rb
@@ -5,37 +5,41 @@ require 'json'
 
 module SmsSender
 
-  URL = "https://staging.api.telstra.com/v1/oauth/token?"\
-      "client_id=#{client_id}&client_secret=#{client_secret}"\
-    "&grant_type=client_credentials&scope=SMS"
+
+
 
   def get_token(c_id, c_secret, rest_client)
     # get an hour-long session token
     resp = rest_client.get "https://staging.api.telstra.com/v1/oauth/token?"\
-      "client_id=#{client_id}&client_secret=#{client_secret}"\
-    "&grant_type=client_credentials&scope=SMS"
+      "client_id=#{c_id}"\
+      "&client_secret=#{c_secret}"\
+      "&grant_type=client_credentials&scope=SMS"
 
     JSON.parse(resp)["access_token"]
   end
 
-  def send_sms(number, body, rest_client)
+  def send_sms( token, number, body, rest_client)
       header =  {authorization: "Bearer #{token}",
                  "Content-Type" => "application/json",
                  "Accept" => "application/json"}
       #and send the message, with the header to Telstra
-      rest_client.post "https://staging.api.telstra.com/v1/sms/messages",
+        rest_client.post "https://staging.api.telstra.com/v1/sms/messages", 
         {to: number, body: body }.to_json, header
-    end
-    module_function :send_sms
+  end
+
 
   end
-  #sends 'hello' to James's number (please don't bombard me)
-  class TestSender
+  
+  class ExampleSender
     include SmsSender
-    def trySending
-      get_token(
-      send_sms("0400400269", "Hello")
+
+    CLIENT_ID = "jiUppg79kSishjRu6OSkk8k2LCTm7VJE" 
+    CLIENT_SECRET = "LLPYNnRboAj1pBtQ"
+    
+    def trySending(number, body )
+      token = get_token(CLIENT_ID ,CLIENT_SECRET, RestClient)
+      send_sms(token, number, body, RestClient)
     end
   end
 
-  TestSender.new.trySending
+ExampleSender.new.trySending("0400400269", "hello")
--- sms_sender/spec/sms_sender_spec.rb
+++ sms_sender/spec/sms_sender_spec.rb
@@ -6,15 +6,41 @@ describe 'SmsSender' do
   let(:client_secret) { "LLPYNnRboAj1pBtQ" }
   let(:rest_client) { double('RestClient') }
   let(:want) { '{ "asdasd": "asdA" }' }
+  describe '#get_token' do
 
-  describe '#send_sms' do
-
-    it 'should forward the request' do
+    it 'should forward the token request' do
+      url = "https://staging.api.telstra.com/v1/oauth/token?"\
+      "client_id=#{:client_id}"\
+      "&client_secret=#{:client_secret}"\
+      "&grant_type=client_credentials&scope=SMS"
       allow(rest_client).to receive(:get).with(url).once { want }
       got = get_token(client_id, client_secret, rest_client)
       expect(got).to eq(JSON.want)
     end
 
+
+
+
   end
+
+
+  let(:destination) {"https://staging.api.telstra.com/v1/sms/messages"}
+  describe '#send_sms' do
+
+    it 'should post the message using the token' do
+      token = "1234567890"
+      body = "hello"
+      number = "0400400269"
+      json_string = {number: number, body: body}.to_json
+      header =  {authorization: "Bearer #{token}",
+                 "Content-Type" => "application/json",
+                 "Accept" => "application/json"}
+
+      expect(rest_client).to receive(:post).with(
+        :destination, json_string, header).once {"{:id asygsevfka"}
+      send_sms( token, number, body, rest_client)
+      
+      
+
 
 end
