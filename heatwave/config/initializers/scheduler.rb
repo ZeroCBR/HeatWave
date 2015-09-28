a44e9d1c40c2ff984948a5bc6274251a4be2f066
Ray <xraycbr@gmail.com>
2015-09-28 13:28:52 +1000

    change pull sequence

--- heatwave/config/initializers/scheduler.rb
+++ heatwave/config/initializers/scheduler.rb
@@ -5,8 +5,8 @@ PULL_WEATHER = 'puller'
 PULL_LOCATION = 'puller location'
 
 scheduler.in '3s' do
-  system('cd ' + PULLER_PATH + ' && ' + PULL_WEATHER)
   system('cd ' + PULLER_PATH + ' && ' + PULL_LOCATION)
+  system('cd ' + PULLER_PATH + ' && ' + PULL_WEATHER)
 end
 
 scheduler.every '87300s' do
