44190533caaa2afba880b6a311c8b96885cbf2c3
Ray <xraycbr@gmail.com>
2015-09-28 12:46:31 +1000

    correct puller path
    correct mistake
    
    rubocop correct

--- heatwave/config/initializers/scheduler.rb
+++ heatwave/config/initializers/scheduler.rb
@@ -5,10 +5,10 @@ PULL_WEATHER = 'puller'
 PULL_LOCATION = 'puller location'
 
 scheduler.in '3s' do
-  system('cd' + PULLER_PATH + ' && ' + PULL_WEATHER)
-  system('cd' + PULLER_PATH + ' && ' + PULL_LOCATION)
+  system('cd ' + PULLER_PATH + ' && ' + PULL_WEATHER)
+  system('cd ' + PULLER_PATH + ' && ' + PULL_LOCATION)
 end
 
 scheduler.every '87300s' do
-  system('cd' + PULLER_PATH + ' && ' + PULL_WEATHER)
+  system('cd ' + PULLER_PATH + ' && ' + PULL_WEATHER)
 end
