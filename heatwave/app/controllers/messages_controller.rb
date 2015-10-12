e73bcac53a505638cb90e3a9d14cd11efa55d212
James McMahon <whichoneiwonder@gmail.com>
2015-10-12 19:00:11 +1100

    sorting messages by email and name working
    also added message log to navbar

--- heatwave/app/controllers/messages_controller.rb
+++ heatwave/app/controllers/messages_controller.rb
@@ -3,19 +3,25 @@
 # a limited version of the standard controller created by the scaffold
 # only allows for viewing throught the rails app
 class MessagesController < ApplicationController
-  before_action :admin_user!
-
+  before_filter :authenticate_user!
   helper_method :sort_column, :sort_direction
   # GET /messages
   # GET /messages.json
   def index
-    @messages = Message.order(sort_column + ' ' + sort_direction)
+    @messages = Message.joins(
+      'INNER JOIN users ON users.id = messages.user_id'
+      ).order(sort_column + ' ' + sort_direction)
+
+    return @messages if current_user.admin_access?
+    @messages = @messages.select { |x| x.user == current_user }
   end
 
   # GET /messages/1
   # GET /messages/1.json
   def show
     set_message
+    #return unless current_user.admin_access?
+    #redirect_to action: index unless @message.user == current_user
   end
 
   private
@@ -26,7 +32,8 @@ class MessagesController < ApplicationController
   end
 
   def sort_column
-    Message.column_names.include?(params[:sort]) ? params[:sort] : 'send_time'
+    (Message.column_names + %w(f_name, l_name, email))
+      .include?(params[:sort]) ? params[:sort] : 'send_time'
   end
 
   def sort_direction
