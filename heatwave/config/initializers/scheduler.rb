e7fd3f313920e8ce2501c8fbe515ebe6176ea8ab
Ned Pummeroy <nedpummeroy@gmail.com>
2015-10-15 14:09:56 +1100

    Added the messenger to the scheduler.
    * Made messages be sent once per day, an hour after
      the scheduled weather pull.
    * Made weather be pulled four times per day, in case
      the forecast changes or the BoM is down.
    * Added documentation explaining what each scheduled task is
      and why it's scheduled in that way.

--- heatwave/config/initializers/scheduler.rb
+++ heatwave/config/initializers/scheduler.rb
@@ -1,14 +1,26 @@
 require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
-PULLER_PATH = '../puller/'
 PULL_WEATHER = 'puller'
 PULL_LOCATION = 'puller location'
+SEND_MESSAGES = 'messenger'
 
 scheduler.in '3s' do
-  system('cd ' + PULLER_PATH + ' && ' + PULL_LOCATION)
-  system('cd ' + PULLER_PATH + ' && ' + PULL_WEATHER)
+  system("cd #{ENV['PULLER_HOME']} && #{PULL_LOCATION}")
+  system("cd #{ENV['PULLER_HOME']} && #{PULL_WEATHER}")
 end
 
-scheduler.every '87300s' do
-  system('cd ' + PULLER_PATH + ' && ' + PULL_WEATHER)
+# Pull weather data four times daily.
+# Don't just pull once, since the site might go down, and the forecast may
+# change.
+# Don't repeatedly try to repull constantly until it works, since we could
+# end up DDoSing ourselves.
+scheduler.every '6h' do
+  system("cd #{ENV['PULLER_HOME']} && #{PULL_WEATHER}")
+end
+
+# Run the messenger a while after the puller, for load balancing.
+scheduler.in '1h' do
+  scheduler.every '24h' do
+    system("cd #{ENV['MESSENGER_HOME']} && bundle exec #{SEND_MESSAGES}")
+  end
 end
