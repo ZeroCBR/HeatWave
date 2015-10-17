2024b1e6d9ca0b9f49613b91dc073127cb6be2a2
Ned Pummeroy <nedpummeroy@gmail.com>
2015-10-18 01:48:34 +1100

    DRY'd up the user views and fixed test case.
    * Pushed the logic for custom User labels back into UserFormBuilder.
    * Updated the test cases for UserFormBuilder to work with the new
      bootstrap style labels.

--- heatwave/app/helpers/user_helper.rb
+++ heatwave/app/helpers/user_helper.rb
@@ -3,4 +3,16 @@ module UserHelper; end
 
 ## Standardises custom form labels for User forms.
 class UserFormBuilder < ActionView::Helpers::FormBuilder
+  def label(attribute, options = {})
+    case attribute
+    when :f_name
+      super attribute, 'Given Name', options
+    when :l_name
+      super attribute, 'Family Name', options
+    when :admin_access
+      super attribute, 'Administrator?', options
+    else
+      return super
+    end
+  end
 end
--- heatwave/test/helpers/user_helper_test.rb
+++ heatwave/test/helpers/user_helper_test.rb
@@ -3,14 +3,24 @@ require 'test_helper'
 # Test the feedback controller with a normal user.
 class UserFormBuilderTest < ActionView::TestCase
   setup do
-    @it = UserFormBuilder.new 'User', User.new, nil, {}
+    @template = Object.new
+    @template.extend ActionView::Helpers::FormHelper
+    @template.extend ActionView::Helpers::FormOptionsHelper
+    @it = UserFormBuilder.new 'User', User.new, @template, {}
   end
 
   test 'UserHelper#label(:f_name) should return "Given Name"' do
-    assert_equal 'Given Name', @it.label(:f_name)
+    assert_equal '<label for="User_f_name">Given Name</label>',
+                 @it.label(:f_name)
   end
 
   test 'UserHelper#label(:l_name) should return "Family Name"' do
-    assert_equal 'Family Name', @it.label(:l_name)
+    assert_equal '<label for="User_l_name">Family Name</label>',
+                 @it.label(:l_name)
+  end
+
+  test 'UserHelper#label(:admin_access) should return "Administrator?"' do
+    assert_equal '<label for="User_admin_access">Administrator?</label>',
+                 @it.label(:admin_access)
   end
 end
